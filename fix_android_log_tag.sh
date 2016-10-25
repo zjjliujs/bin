2c255293520d954bd6ddf9129aa057855e27b629
LiujinsongRR <liujinsong@rockrobo.com>
2016-10-25 14:59:25 +0800

    Complete prepare_rockrobo.sh :-)

--- fix_android_log_tag.sh
+++ fix_android_log_tag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 find . -name '*.java'|xargs sed -i 's/\/\/\(private static final String LOG_TAG = Device.MODEL\)/\1/'
 
-find . -name '*.java'|xargs sed -i 's/\(^[ \t]*\)\(private static final String LOG_TAG = \"ljs\"\)/\1\/\/\2/'
+find . -name '*.java'|xargs sed -i 's/\(^[ \t]*\)\(private static final String LOG_TAG = (\"ljs:\"\)/\1\/\/\2/'
