aa812b226ef3038856071a6b1ed7f4f0327bc68d
LiujinsongRR <liujinsong@rockrobo.com>
2017-05-24 16:40:04 +0800

    ruby script

---
+++ rubyby_cp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: cp_rockrobo <mode> <version>"
+	exit 11
+fi
+
+version="$2"
+model="$1"
+
+filename="rockrobo_vacuum-${version}-0.1.${version}-release.apk"
+
+srcdir="plugProject/rockrobo_vacuum/build/outputs/apk"
+destdir="/cygdrive/c/work/apk/rr-${model}"
+
+echo cp "${srcdir}/${filename}" "${destdir}/"
+cp "${srcdir}/${filename}" "${destdir}/"
+echo "Done!"
+
