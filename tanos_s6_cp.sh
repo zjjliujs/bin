b2b671f230f90748d8605cd83bbd63c473e15ecf
zjjljs <liujinsong@rockrobo.com>
2018-12-14 17:11:15 +0800

    add tanos-s6 script

---
+++ tanos_s6_cp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: tanos_s6_cp <mode> <version>"
+	exit 11
+fi
+
+version="$2"
+model="$1"
+
+filename="rockrobo_vacuum-${version}-0.1.${version}-release.apk"
+
+srcdir="plugProject/rockrobo_vacuum/build/outputs/apk/release"
+destdir="/cygdrive/c/work/apk/tanos-s6-${model}"
+
+echo cp "${srcdir}/${filename}" "${destdir}/"
+cp "${srcdir}/${filename}" "${destdir}/"
+echo "Done!"
+
