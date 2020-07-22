2dbfdc48cd862aad9b066fcbe086151bd41f668f
zjjljs <zjj_ljs@163.com>
2020-07-22 09:49:18 +0800

    改进cscope_build.sh, 加路径参数

--- cscope_build.sh
+++ cscope_build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+path=.
+if [ $# -gt 0 ]
+then
+  path=$@
+fi
+
 currentPath=$(pwd | sed 's$\/$\\\/$g')
 echo $currentPath
-find . -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp"| sed "s/^\./$currentPath/"> cscope.files
+find $path -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp"| sed "s/^\./$currentPath/"> cscope.files
 cscope -bkq -i cscope.files
 ctags -R
