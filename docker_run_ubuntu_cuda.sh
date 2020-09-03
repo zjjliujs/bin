6e51173b14ed06f1bbf594a7eab1bc9380c094c9
Liu Jinsong <liujinsong@dev.com>
2020-09-03 16:28:33 +0800

    修改脚本

--- docker_run_ubuntu_cuda.sh
+++ docker_run_ubuntu_cuda.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+if [ $# -ne 2 ]
+then 
+    echo "Usage: <command> <image name> <image tag>"
+    exit -1
+fi
+
+imgName=$1
+imgTag=$2
+
+echo run docker image ${imgName}:${imgTag}
+
 docker run --gpus all \
     --env NVIDIA_DRIVER_CAPABILITIES="utility,compute" \
     -v /home/liujinsong/quantize_ljs/data:/mnt/data \
     -v /home/liujinsong/quantize_ljs/model:/mnt/model \
+    -v /home/liujinsong/cuda:/mnt/cuda \
+    -v /home/liujinsong/caffes/caffe-quant-git:/mnt/caffe-quant \
+    -v /home/liujinsong/caffes/caffe-cpu:/mnt/caffe-cpu \
+    -v /home/liujinsong/download:/mnt/download \
     -w /root \
-    -it ubuntu-18.04-tsm:cuda
+    -it ${imgName}:${imgTag}
+
---
+++ docker_run_ubuntu_ljs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run --gpus all \
+    --env NVIDIA_DRIVER_CAPABILITIES="utility,compute" \
+    -v /home/liujinsong/quantize_ljs/data:/mnt/data \
+    -v /home/liujinsong/quantize_ljs/model:/mnt/model \
+    -w /root \
+    -it ubuntu-18.04-tsm:ljs
+
